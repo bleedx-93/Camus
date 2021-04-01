f0016321f807f003f597b38b3f611b1fba42efcc
mkhorram@protonmail.com <mkhorram@protonmail.com>
2021-04-01 13:42:07 +0430

    Fix bug

--- Camus/Camus.cpp
+++ Camus/Camus.cpp
@@ -11,11 +11,9 @@ using namespace std;
 
 int main(int argc , char **argv)
 {
-
    const char* IP_Address;
    string str_obj(argv[1]);
    IP_Address = &str_obj[0];
-
    int Port = atoi(argv[2]);
 
    Mouse_Movement();
